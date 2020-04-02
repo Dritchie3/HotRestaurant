40056db4f740b6458fcf3c655eed8c62b16d9235
Ben Loney <benloney@gmail.com>
2020-04-01 21:36:25 -0500

    updated

--- server.js
+++ server.js
@@ -1,23 +1,5 @@
 var express = require("express");
-<<<<<<< HEAD
-var bodyParser = require("body-parser");
 
-var app = express();
-
-
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
-=======
 var path = require("path");
 var app = express();
 var PORT = 3000;
@@ -43,4 +25,3 @@ app.get("/", function(req, res) {
   app.get("/add", function(req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
   });
->>>>>>> 14886d1119bcf1ce5389872dc55f4b6427a96ae6
